ce7c9dc7d65ebe763b7381dd6e7ac3a08a121946
Rafael Caldeira <rafazeon@gmail.com>
2022-09-30 15:29:21 -0300

    feat: tuple type

--- src/test.ts
+++ src/test.ts
@@ -4,15 +4,17 @@ let c: string;
 
 let d: Array<string>;
 
-type Permissions = 'admin' | 'user' | 'manager'
+type Pms = 'admin' | 'user' | 'manager'
+
+type TuplePermission = [Pms, Pms]
 
 // Generic Params
-type BasicUser<A = boolean, P = string[]> = {
+type BasicUser<A = boolean, P = TuplePermission> = {
   name: string;
   surname: string;
   age: number;
   isAdmin: A;
-  permissions?: P[];
+  permissions?: P;
 }
 
 type AdvancedUser = {
@@ -22,31 +24,23 @@ type AdvancedUser = {
 // Intersection Params
 type FullUser<A = boolean, P = string[]> = BasicUser<A, P> & AdvancedUser
 
-const user: FullUser<boolean, Permissions> = {
+const user: FullUser<boolean, TuplePermission> = {
   name: "Rafael",
   surname: "Oliveira",
   age: 30,
   isAdmin: true,
   account: 100,
-  permissions: ['admin', 'user', 'manager']
-};
-
-const user2: BasicUser<number> = {
-  name: "Evan",
-  surname: "Noah",
-  age: 31,
-  isAdmin: 1,
-  permissions: ['manager', 'operator', 'instructor']
+  permissions: ['admin', 'user']
 };
 
 // Generic Params
-const usersArray: BasicUser[] = [user,user,user]
+const usersArray: FullUser[] = [user,user,user]
 
 function getFirst<T>(arr: T[]): T {
     return arr[0];
 }
 
-getFirst<BasicUser>(usersArray);
+getFirst<FullUser>(usersArray);
 
 type MathFunc = (a: number, b: number) => number;
 
